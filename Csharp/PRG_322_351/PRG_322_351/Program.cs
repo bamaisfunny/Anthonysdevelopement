74ab00ab7e880fd8002da22377066ab49bf28577
bamaisfunny <theantman15@yahoo.com>
2017-01-25 18:04:36 -0800

    initialprg322

---
+++ Csharp/PRG_322_351/PRG_322_351/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1_prg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentRoster s = new StudentRoster();
+            foreach (KeyValuePair<int, Student> kvp in s.studentDictionary)
+            {
+                kvp.Value.Debug();
+            }
+        }
+    }
+}
