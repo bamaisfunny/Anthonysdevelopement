abdd522982dd6c2cdaac7c2bc548490c0910a705
bamaisfunny <theantman15@yahoo.com>
2017-01-26 20:13:17 -0800

    Converter
    Converter

---
+++ unitConverter/Converter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace unitConverter
+{
+   
+    public class Converter
+    {
+        public static Dictionary<string, Converter> conversions = new Dictionary<string, Converter>();
+
+        public static double Gs2Kgs(double input)
+        {
+            return input / 1000;
+        }
+        public static double Gs2Lbs(double input)
+        {
+            return input * 453.592;
+        }
+        public static double Gs2Ozs(double input)
+        {
+            return input * 0.035274;
+        }
+        public static double Gs2Gs(double input)
+        {
+            return 1 * input; 
+        }
+
+    }
+}
