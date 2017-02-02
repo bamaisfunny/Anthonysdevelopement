f2d9c21191dc520f33d483456ee7a6c5459b64fc
bamaisfunny <theantman15@yahoo.com>
2017-02-01 18:52:14 -0800

    Serializer1
    Serializer1

---
+++ Csharp/PRG_322_351/Serializer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.StreamWriter tw = new System.IO.StreamWriter("C:\\Users\\CCSDUser\\Documents\\PracticeFileWriting\\Example.txt"); 
+            tw.WriteLine("Age = " + 28);
+            tw.Close();
+
+            System.IO.StreamReader tr = new System.IO.StreamReader("C:\\Users\\CCSDUser\\Documents\\PracticeFileWriting\\Example.txt");
+            String line_read = tr.ReadLine();
+            string[] age_seperator = { "Age = " };
+            string[] value_extracted = line_read.Split(age_seperator, StringSplitOptions.RemoveEmptyEntries);
+            int age = Convert.ToInt32(value_extracted[0]);
+            Console.WriteLine("Age (read back = " + age);
+            Console.ReadKey();
+        }
+      
+    }
+}
